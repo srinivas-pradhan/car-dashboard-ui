616535a4205d5c2c755dae7ac3e0e67d5af9bbfc
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-07-29 11:47:32 -0400

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,10 @@
 const Navbar = () => {
     return (
-        <div className="fixed h-16 left-0 top-0 w-screen bg-white">
-
+        <div className="flex flex-row justify-end space-x-1 h-16 left-0 top-0 w-screen bg-white ">
+            <ul>
+                <li>SearchBar</li>
+                <li>User Mgmt</li>
+            </ul>
         </div>
     );
 }
