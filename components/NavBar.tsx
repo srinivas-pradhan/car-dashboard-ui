93bc077b9a35972808500b13ee1fb9ba63a2adbd
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-08-02 11:46:40 -0400

    Finish SideBar and Navbar Clerk SignIn

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,16 @@
 'use client';
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
     return (
-        <div className="flex flex-row justify-end space-x-1 h-16 left-0 top-0 w-screen bg-white ">
-            <ul>
-                <li>SearchBar</li>
-                <li>User Mgmt</li>
+        <div className="h-16 w-screen bg-white right-0">
+            <ul className="flex flex-row justify-end md:space-x-[66%]">
+                <li className="mt-5">SearchBar</li>
+                <li className="mt-5 pr-8">
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </li>
             </ul>
         </div>
     );
