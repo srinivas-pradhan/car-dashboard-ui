a479779ec9be95fc4ffe638d80546b872ee8759b
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-08-12 15:48:27 -0400

    Update GridLayout.tsx

--- components/Dashboard/GridLayout.tsx
+++ components/Dashboard/GridLayout.tsx
@@ -2,7 +2,7 @@
 
 const  GridLayout = () => {
     return (  
-        <div className="pl-[20%] flex justify-center ">
+        <div className="pl-[20%] flex justify-center">
             <ul className="grid grid-cols-4 ml-10 mr-10 mt-4 gap-x-10 gap-y-6">
                 <li className="rounded-xl bg-teal-500 w-60 h-48">Text</li>
                 <li className="rounded-xl bg-teal-500 w-60 h-48">Text</li>
@@ -10,11 +10,14 @@ const  GridLayout = () => {
                 <li className="rounded-xl bg-teal-500 w-60 h-48">Text</li>
                 <li className="col-span-2 rounded-xl bg-teal-500 w-full h-64">Text</li>
                 <li className="col-span-2 rounded-xl bg-teal-500 w-full h-64">Text</li>
-                <li className="rounded-xl bg-teal-500 w-72 h-40">Text</li>
-                <li className="rounded-xl bg-teal-500 w-72 h-40">Text</li>
-                <li className="rounded-xl bg-teal-500 w-72 h-40">Text</li>
+                <li className="rounded-xl bg-teal-500 w-60 h-40">Text</li>
+                <li className="rounded-xl bg-teal-500 w-60 h-40">Text</li>
+                <li className="rounded-xl bg-teal-500 w-60 h-40">Text</li>
+                <li className="rounded-xl bg-teal-500 w-60 h-40">Text</li>
             </ul>
         </div>
+
+
     );
 }
  
